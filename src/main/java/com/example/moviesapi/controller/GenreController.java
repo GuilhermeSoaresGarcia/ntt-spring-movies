f905e595cb73b0a0bc45b081279cdc2e62268dc7
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-27 18:31:30 -0300

    feat: implementada camadas GenreController e GenreService com os métodos GET, POST, PUT e DELETE

---
+++ src/main/java/com/example/moviesapi/controller/GenreController.java
@@ -0,0 +1,51 @@
+package com.example.moviesapi.controller;
+
+import com.example.moviesapi.model.entity.Genre;
+import com.example.moviesapi.service.GenreService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
+
+@RestController
+@RequestMapping("genre")
+public class GenreController {
+
+  @Autowired
+  GenreService genreService;
+
+  @GetMapping("/list")
+  public List<Genre> getAllGenres() {
+    return genreService.getAllGenres();
+  }
+
+  @GetMapping("/{id}")
+  public Genre getGenreById(@PathVariable Long id) {
+    return genreService.getGenreById(id);
+  }
+
+  @PostMapping("/save")
+  public Genre registerGenre(@RequestBody Genre genre) {
+    return genreService.registerGenre(genre);      
+  }
+
+  @PutMapping("/update")
+  public Genre updateGenre(@RequestBody Genre genre) {
+      return genreService.updateGenre(genre);
+  }
+
+  @DeleteMapping("/delete/{id}")
+  public String deleteGenre(@PathVariable Long id){
+    return genreService.deleteGenre(id);
+  }
+}
