a34d11f6cab474bb508cf6769d423615bb1d672b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-27 22:04:12 -0300

    docs: criação de um README.md para o GitHub e arquivos de migrations e endpoints da api

---
+++ README.md
@@ -0,0 +1,17 @@
+# NTT Spring Movies
+
+- Projeto desenvolvido em framework [Spring](https://github.com/angular/angular-cli), versão 3.2.4
+- Utilizadas bibliotecas Spring Web, Spring Devtools, Spring Validation, driver MySQL e Lombok
+
+O objetivo deste projeto foi desensvolver uma API rest utilizando o Spring e conectando-se a um bando de dados - neste caso, o MySQL.
+
+## Como executar
+
+Clone o projeto, execute `mvn install` para certificar de que as dependências foram corretamente instaladas e rode `mvn spring-boot:run` para executar o projeto em modo desenvolvimento. Os end-points tem como raiz a url base do Spring (normalmente `http://localhost:8080/`) mais alguma subrota.
+
+É possível importar os end-points que estão disponíveis no JSON `ntt-spring-movies-endpoints.json` para os clientes http [Bruno](https://www.usebruno.com/) ou [Postman](https://www.postman.com/) e migrations iniciais para o banco de dados estão disponíveis no arquivo migrations.sql.
+
+
+## Observações
+
+- Eu já havia estudado Spring anteriormente e, por mais que eu goste bastante do framework, acho que com o tempo curto que tivemos para desenvolver o projeto, algumas coisas acabaram passando batidas - e algumas outras poderiam ser melhoradas, mas de qualquer modo, os end-points estão todos acessíveis.
---
+++ migrations.sql
@@ -0,0 +1,46 @@
+INSERT INTO movies (title) VALUES ('The Matrix');
+INSERT INTO movies (title) VALUES ('The Silence of the Lambs');
+INSERT INTO movies (title) VALUES ('Back to the Future');
+INSERT INTO movies (title) VALUES ('Cidade de Deus');
+INSERT INTO movies (title) VALUES ('OldBoy');
+INSERT INTO movies (title) VALUES ('Inception');
+
+INSERT INTO genres (genre_name) VALUES ('Action');
+INSERT INTO genres (genre_name) VALUES ('Drama');
+INSERT INTO genres (genre_name) VALUES ('Comedy');
+INSERT INTO genres (genre_name) VALUES ('Science Fiction');
+INSERT INTO genres (genre_name) VALUES ('Adventure');
+INSERT INTO genres (genre_name) VALUES ('Horror');
+
+INSERT INTO actors (name) VALUES ('Anthony Hopkins');
+INSERT INTO actors (name) VALUES ('Jodie Foster');
+INSERT INTO actors (name) VALUES ('Ted Levine');
+INSERT INTO actors (name) VALUES ('Michael J. Fox');
+INSERT INTO actors (name) VALUES ('Christopher Lloyd');
+INSERT INTO actors (name) VALUES ('Carrie-Anne Moss');
+INSERT INTO actors (name) VALUES ('Keanu Reeves');
+INSERT INTO actors (name) VALUES ('Laurence Fishburne');
+INSERT INTO actors (name) VALUES ('Hugo Weaving');
+INSERT INTO actors (name) VALUES ('Leonardo DiCaprio');
+INSERT INTO actors (name) VALUES ('Cillian Murphy');
+INSERT INTO actors (name) VALUES ('Marion Cotillard');
+INSERT INTO actors (name) VALUES ('Michael Caine');
+INSERT INTO actors (name) VALUES ('Tom Berenger');
+INSERT INTO actors (name) VALUES ('Ken Watanabe');
+INSERT INTO actors (name) VALUES ('Elliot Page');
+INSERT INTO actors (name) VALUES ('Tom Hardy');
+INSERT INTO actors (name) VALUES ('Joseph Gordon-Levitt');
+
+INSERT INTO studios (name, country) VALUES ('Universal', 'Estados Unidos');
+INSERT INTO studios (name, country) VALUES ('Warner Bros.', 'Estados Unidos');
+INSERT INTO studios (name, country) VALUES ('Paramount', 'Estados Unidos');
+INSERT INTO studios (name, country) VALUES ('Studio Ghibli', 'Japao');
+INSERT INTO studios (name, country) VALUES ('Estudios Globo', 'Brasil');
+
+INSERT INTO directors (name) VALUES ('Cristopher Nolan');
+INSERT INTO directors (name) VALUES ('Steven Spielberg');
+INSERT INTO directors (name) VALUES ('Fernando Meirelles');
+INSERT INTO directors (name) VALUES ('Park Chan-wook');
+INSERT INTO directors (name) VALUES ('Lilly Wachowski');
+INSERT INTO directors (name) VALUES ('Lana Wachowski');
+INSERT INTO directors (name) VALUES ('Jonathan Demme');
