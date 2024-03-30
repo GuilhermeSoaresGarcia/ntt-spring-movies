5aa7ca66383ffbab1f3959e441e1e7376ac32de8
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-30 13:02:06 -0300

    feat: tratamento de exceções na camada FranchiseController

--- src/main/java/com/example/moviesapi/controller/FranchiseController.java
+++ src/main/java/com/example/moviesapi/controller/FranchiseController.java
@@ -6,11 +6,13 @@ import com.example.moviesapi.service.FranchiseService;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -29,21 +31,41 @@ public class FranchiseController {
 
   @GetMapping("/{id}")
   public Franchise getFranchiseById(@PathVariable Long id) {
-    return franchiseService.getFranchiseById(id);
+    try {
+      return franchiseService.getFranchiseById(id);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.NOT_FOUND, e.getMessage());
+    }
   }
 
   @PostMapping("/save")
   public Franchise registerFranchise(@RequestBody Franchise franchise) {
-    return franchiseService.registerFranchise(franchise);
+    try {
+      return franchiseService.registerFranchise(franchise);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 
   @PutMapping("/update")
   public Franchise updateFranchise(@RequestBody Franchise franchise) {
-    return franchiseService.updateFranchise(franchise);
+    try {
+      return franchiseService.updateFranchise(franchise);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 
   @DeleteMapping("/delete/{id}")
   public String deleteFranchise(@PathVariable Long id) {
-    return franchiseService.deleteFranchise(id);
+    try {
+      return franchiseService.deleteFranchise(id);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.NOT_FOUND, e.getMessage());
+    }
   }
 }
