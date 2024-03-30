6fdae0a6c04a003462483f3c278ace16f53d5a43
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-30 11:33:39 -0300

    docs: implementação do Swagger para documentação facilitada das rotas

--- src/main/java/com/example/moviesapi/MoviesapiApplication.java
+++ src/main/java/com/example/moviesapi/MoviesapiApplication.java
@@ -3,7 +3,11 @@ package com.example.moviesapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "NTT Movies Spring API", version = "1", description = "API desenvolvida com Spring para a academia NTT"))
 public class MoviesapiApplication {
 
 	public static void main(String[] args) {
