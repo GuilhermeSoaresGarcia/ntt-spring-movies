6830f44e77c5937d3a0f7e435b8fac81e10ce2f0
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-03-30 13:03:58 -0300

    feat: tratamento de exceções na camada GenreController

--- src/main/java/com/example/moviesapi/controller/GenreController.java
+++ src/main/java/com/example/moviesapi/controller/GenreController.java
@@ -8,15 +8,16 @@ import io.swagger.v3.oas.annotations.Hidden;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
-
 
 @RestController
 @Hidden
@@ -33,21 +34,41 @@ public class GenreController {
 
   @GetMapping("/{id}")
   public Genre getGenreById(@PathVariable Long id) {
-    return genreService.getGenreById(id);
+    try {
+      return genreService.getGenreById(id);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.NOT_FOUND, e.getMessage());
+    }
   }
 
   @PostMapping("/save")
   public Genre registerGenre(@RequestBody Genre genre) {
-    return genreService.registerGenre(genre);      
+    try {
+      return genreService.registerGenre(genre);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 
   @PutMapping("/update")
   public Genre updateGenre(@RequestBody Genre genre) {
+    try {
       return genreService.updateGenre(genre);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.BAD_REQUEST, e.getMessage());
+    }
   }
 
   @DeleteMapping("/delete/{id}")
-  public String deleteGenre(@PathVariable Long id){
-    return genreService.deleteGenre(id);
+  public String deleteGenre(@PathVariable Long id) {
+    try {
+      return genreService.deleteGenre(id);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.NOT_FOUND, e.getMessage());
+    }
   }
 }
