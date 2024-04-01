0ec7386a92dae6a321a8e47c3611f48bae8c50cc
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-01 20:15:29 -0300

    feat: alterada a tipagem de retorno na interface UserFacade para o uso do UserDTO

--- src/main/java/com/example/moviesapi/facade/UserFacade.java
+++ src/main/java/com/example/moviesapi/facade/UserFacade.java
@@ -2,16 +2,17 @@ package com.example.moviesapi.facade;
 
 import java.util.List;
 
+import com.example.moviesapi.dto.UserDTO;
 import com.example.moviesapi.model.entity.User;
 
 public interface UserFacade {
-  List<User> getAllUsers();
+  List<UserDTO> getAllUsers();
 
-  User getUserById(Long id);
+  UserDTO getUserById(Long id);
 
-  User registerUser(User user);
+  UserDTO registerUser(User user);
 
-  User updateUser(User user);
+  UserDTO updateUser(User user);
 
   String deleteUser(Long id);
 
