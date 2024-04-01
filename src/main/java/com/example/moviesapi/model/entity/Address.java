b40f23f5d7aafa46310a302c17c40a83d7d3d07b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-01 20:21:13 -0300

    feat: criada classe Address que será utilizada para salvar os dados de endereço do usuário após consumo da API dos correios

--- src/main/java/com/example/moviesapi/model/entity/Address.java
+++ src/main/java/com/example/moviesapi/model/entity/Address.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "adresses")
 @Data
 @NoArgsConstructor
 public class Address {
@@ -16,10 +20,11 @@ public class Address {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
+  @NotNull
+  @NotBlank
   private String street;
 
   private Integer number;
 
   private String neighborhood;
-
 }
