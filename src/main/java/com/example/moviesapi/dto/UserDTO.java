a9261ed13fb714996466f5d1c7bce945ce9aedbf
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-01 17:06:11 -0300

    feat: implementa UserDTO

---
+++ src/main/java/com/example/moviesapi/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.example.moviesapi.dto;
+
+import com.example.moviesapi.model.entity.Address;
+import com.example.moviesapi.model.entity.Movie;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String username;
+    private String email;
+    private List<Movie> favorites;
+    private Address address;
+}
