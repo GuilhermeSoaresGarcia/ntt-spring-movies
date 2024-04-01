1032df35c78451d6fb38617049f390bd7f5940fd
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-01 17:05:52 -0300

    feat: implementado service UserMapperService que converte o User (e também uma lista de User) em UserDTO utilizando a biblioteca ModelMapper

---
+++ src/main/java/com/example/moviesapi/service/UserMapperService.java
@@ -0,0 +1,26 @@
+package com.example.moviesapi.service;
+
+import com.example.moviesapi.dto.UserDTO;
+import com.example.moviesapi.model.entity.User;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserMapperService {
+
+  private final ModelMapper modelMapper;
+
+  public UserMapperService(ModelMapper modelMapper) {
+    this.modelMapper = modelMapper;
+  }
+
+  public UserDTO mapUserToUserDTO(User user) {
+    return modelMapper.map(user, UserDTO.class);
+  }
+
+  public List<UserDTO> mapUsersToUserDTOs(List<User> users) {
+    return users.stream().map(user -> mapUserToUserDTO(user)).toList();
+  }
+}
