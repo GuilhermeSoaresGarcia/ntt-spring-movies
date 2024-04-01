3ec0e67dd88c050d118dc8d1338f6df508f6c4cf
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-01 17:07:14 -0300

    feat: implementa ResultDTO com o padrão de resposta do projeto

---
+++ src/main/java/com/example/moviesapi/dto/ResultDTO.java
@@ -0,0 +1,14 @@
+package com.example.moviesapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResultDTO {
+
+  private String message;
+  private int status;
+}
