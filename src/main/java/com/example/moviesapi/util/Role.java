ff311a9831ddedf4c3f64e7a7c0d9300a3652bfa
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-03 22:15:24 -0300

    feat: criado enum Role que possivelmente será usado na autenticação

---
+++ src/main/java/com/example/moviesapi/util/Role.java
@@ -0,0 +1,17 @@
+package com.example.moviesapi.util;
+
+public enum Role {
+  ADMIN("ROLE_ADMIN"),
+  MANAGER("ROLE_MANAGER"),
+  USER("ROLE_USER");
+
+  private final String name;
+
+  Role(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
