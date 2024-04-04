5046de4bc4c998bcec48ab9dc05d0acfc6cad98f
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-03 22:01:44 -0300

    fix: alterado erro gramatical no nome da tabela (de 'adresses' para 'addresses') e adicionados atributos conforme os da API de consulta de CEP

--- src/main/java/com/example/moviesapi/model/entity/Address.java
+++ src/main/java/com/example/moviesapi/model/entity/Address.java
@@ -1,9 +1,13 @@
 package com.example.moviesapi.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,20 +15,39 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "adresses")
+@Table(name = "addresses")
 @Data
 @NoArgsConstructor
 public class Address {
-
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonIgnore
   private Long id;
 
   @NotNull
   @NotBlank
-  private String street;
+  private String cep;
 
-  private Integer number;
+  @NotNull
+  @NotBlank
+  private String logradouro;
+  private String bairro;
 
-  private String neighborhood;
+  @NotNull
+  @NotBlank
+  private String localidade;
+
+  @NotNull
+  @NotBlank
+  private String uf;
+
+  @OneToOne(mappedBy = "address", 
+            cascade = {
+              CascadeType.DETACH,
+              CascadeType.MERGE,
+              CascadeType.PERSIST,
+              CascadeType.REFRESH }
+              )
+  @JsonIgnore
+  private User user;
 }
