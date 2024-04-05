c1ca1176cbd53ed3847adaf0b033f6999d9af68f
Guilherme Soares Garcia <guilherme.garcia@email.com>
2024-04-05 17:36:00 -0300

    feat: implementação dos filtros por rotas autenticadas/não autenticadas no SecurityConfig

--- src/main/java/com/example/moviesapi/security/SecurityConfig.java
+++ src/main/java/com/example/moviesapi/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.moviesapi.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,31 +11,33 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  // private final SecurityFilter securityFilter;
+  private final SecurityFilter securityFilter;
 
-  // @Autowired
-  // public SecurityConfig(SecurityFilter securityFilter) {
-  // this.securityFilter = securityFilter;
-
-  // }
+  public SecurityConfig(SecurityFilter securityFilter) {
+    this.securityFilter = securityFilter;
+  }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
+      throws Exception {
     return httpSecurity
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(authorize -> authorize
-            // .requestMatchers("").permitAll())
-            .anyRequest().permitAll())
-        // .requestMatchers(HttpMethod.GET, "/user").hasAnyRole("MANAGER", "ADMIN")
-        // .anyRequest().authenticated())
-        // .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+            // .anyRequest().permitAll())
+            .requestMatchers("/v3/**", "/swagger-ui/**").permitAll()
+            .requestMatchers(HttpMethod.GET, "/user/*").permitAll()
+            .requestMatchers(HttpMethod.POST, "/user/*").permitAll()
+            .requestMatchers(HttpMethod.PUT, "/user/*").permitAll()
+            .anyRequest().authenticated())
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 
